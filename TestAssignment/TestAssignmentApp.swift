71262c28d0657ae55cfa64066ae93ebd694e9ed3
artemmihelson <artem.mihelson@gmail.com>
2025-04-26 09:43:44 +0300

    Initial Commit

---
+++ TestAssignment/TestAssignmentApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestAssignmentApp.swift
+//  TestAssignment
+//
+//  Created by Artem Mihelson on 26.04.2025.
+//
+
+import SwiftUI
+
+@main
+struct TestAssignmentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ TestAssignmentTests/TestAssignmentTests.swift
@@ -0,0 +1,17 @@
+//
+//  TestAssignmentTests.swift
+//  TestAssignmentTests
+//
+//  Created by Artem Mihelson on 26.04.2025.
+//
+
+import Testing
+@testable import TestAssignment
+
+struct TestAssignmentTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
